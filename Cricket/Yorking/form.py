a2f1ec4e674e39e490f82397a8b03cf03a005153
Pravalika0106 <gudurupravalika0106@gmail.com>
2020-05-28 14:30:25 +0530

    django form implemented

--- Cricket/Yorking/form.py
+++ Cricket/Yorking/form.py
@@ -4,17 +4,21 @@ from Yorking.models import user
 
 class User_Authentication(forms.Form):
     user_obj=user.objects.all().values()
-    user_name=forms.CharField(max_length=100)
+    user_id=forms.CharField(max_length=100)
     password=forms.CharField(widget=forms.PasswordInput,validators=[validators.MinLengthValidator(5)])
 
 
     def clean(self):
         all_clean_data = super().clean()
-        user_name=all_clean_data['user_name']
-        user_obj=all_clean_data['user_obj']
+        user_obj=user.objects.all().values()
+        user_id=all_clean_data['user_id']
+        password=all_clean_data['password']
+        # user_obj=all_clean_data['user_obj']
         for i in user_obj:
-            if user_name==i['user_name']:
-                raise forms.ValidationError('Username already exists. Please choose an other one')
+            if user_id!=i['user_id']:
+                raise forms.ValidationError('Enter the correct user id')
+            if password!=i['password']:
+                raise forms.ValidationError('Enter the correct password')
 
 
 
